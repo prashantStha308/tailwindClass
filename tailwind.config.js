ecfd10da06591e2f7b1dfc487399dbdf2df174f8
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-08-29 00:28:32 +0545

    tailwind commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ '*.{html,js}' ],
+  darkMode: 'class',
+  content: ['*.{html,js}'],
   theme: {
     extend: {
-      fontFamily: {
+      fontFamily : {
         domine : ['Domine','serif']
+      }, 
+      animation : {
+        'rotating'  : 'rotating 5s linear infinite'
+      }, 
+      keyframes : {
+        rotating : {
+          '0%' : {transform : 'rotate(0.0deg)'}, 
+          '25%' : {transform : 'rotate(90deg)'},
+          '50%' : {transform : 'rotate(180deg)'},
+          '75%' : {transform : 'rotate(270deg)'},
+          '100%' : {transform : 'rotate(3600deg)'},
+          
+        }
       }
     },
   },
   plugins: [],
 }
-
